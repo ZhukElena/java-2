c1d8b0e12224a246de450d326e7f4459f21eea60
lenazhuk <vaitler87@icloud.com>
2022-02-10 19:18:19 +0300

    hw 4

--- src/main/java/com/geekbrains/Main.java
+++ src/main/java/com/geekbrains/Main.java
@@ -2,6 +2,20 @@ package com.geekbrains;
 
 public class Main {
     public static void main(String[] args) {
+        OwnLinkedList<Integer> integerOwnLinkedList = new OwnLinkedList<>();
+        integerOwnLinkedList.addNode(150);
+        integerOwnLinkedList.addNode(347);
+        integerOwnLinkedList.addNode(111);
+        integerOwnLinkedList.addNode(789);
+        integerOwnLinkedList.addNode(222);
+        integerOwnLinkedList.addNode(345);
+
+        integerOwnLinkedList.display();
+        integerOwnLinkedList.remove(0);
+        integerOwnLinkedList.display();
+
+        integerOwnLinkedList.addNode(559, 3);
+        integerOwnLinkedList.display();
     }
 }
 
